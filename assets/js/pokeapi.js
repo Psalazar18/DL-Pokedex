1ff78dc2272f7b060f933b2d1890e621c7b8dea6
Paola <paolasalazar90@gmail.com>
2020-09-02 22:19:06 -0400

    final touches

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -1,41 +1,46 @@
 $("#search").click(function(){
     event.preventDefault();
+
+    var formulario = $('#form');
     var nombre = $('#nombre').val();
-    $.ajax({
-        type: "GET",
-        url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
-        dataType: "json",
-        success: function(data) {
-            $(".respuesta").empty();
-            console.log(data.name);
-            $(".respuesta").append(data.name + "<br>");
-            $(".respuesta").append("Pokemón de tipo: " + data.types[0].type.name);
-            $(".respuesta").css("font-weight", "bold");
+    var expresion = /[a-z]/gim;
+    var expresion2 = /[1-893]/gim;
+    if (nombre.match(expresion) || nombre.match(expresion2) && nombre > 0 && nombre <=893 && nombre != ""){
+        $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${nombre}`,
+            dataType: "json",
+            success: function(data) {
+                $("#respuesta").empty();
+                console.log(data.name);
+                $("#respuesta").append(data.name + "<br>");
+                $("#respuesta").append("Pokemón de tipo: " + data.types[0].type.name);
+                $("#respuesta").css("font-weight", "bold");
 
-            var options = {
-                title: {
-                    text: "Pokemon stat"              
-                },
-                data: [              
-                    {
-                        type: "column",
-                        dataPoints:[
-                            
-                        ]
-                    }
-                ]
+                var options = {
+                    title: {
+                        text: "Pokemon stat"              
+                    },
+                    data: [              
+                        {
+                            type: "column",
+                            dataPoints:[]
+                        }
+                    ]
+                }
+
+                for (var i = 0; i < data.stats.length; i++) {
+                    options.data[0].dataPoints.push({label: data.stats[i].stat.name, y:data.stats[i].base_stat});
+                }
+
+                $("#chartContainer").CanvasJSChart(options);
+                $("#chartContainer").show();
+            },
+            error: function(error){
+                console.error(error.responseText);
             }
-
-            for (var i = 0; i < data.stats.length; i++) {
-                options.data[0].dataPoints.push({label: data.stats[i].stat.name, y:data.stats[i].base_stat});
-            }
-
-            $("#chartContainer").CanvasJSChart(options);
-            $("#chartContainer").show("slow");
-        },
-        error: function(error){
-            console.error(error.responseText);
-        }
-    });  
- });   
-    
+        });  
+    }else{
+        alert("Ingrese un número del 1 al 893 o ingrese el nombre correctamente!")
+    }
+ }); 
